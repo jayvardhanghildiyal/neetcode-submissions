6282c9e40b76bcb40a876b94d4d315ac3dee9f82
Jayvardhan Ghildiyal <jayvghildiyal2016@gmail.com>
2026-09-23 17:03:10 +0930

    Add: valid-palindrome-ii - submission-3

---
+++ Data Structures & Algorithms/valid-palindrome-ii/submission-3.java
@@ -0,0 +1,55 @@
+// check both possibilities
+// either we move pointer to the left
+// or we move it to the right
+class Solution {
+    public boolean validPalindrome(String s) {
+        
+        int left = 0, right = s.length() - 1, skip = 1;
+        boolean leftShift = true;
+        while (left <= right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left += 1;
+                right -= 1;
+            } else if (skip == 1) {
+                left += 1;
+                skip -= 1;
+            } else {
+                leftShift = false;
+                break;
+            }
+        }
+
+        left = 0;
+        right = s.length() - 1;
+        skip = 1;
+        boolean rightShift = true;
+        while (left <= right) {
+            if (s.charAt(left) == s.charAt(right)) {
+                left += 1;
+                right -= 1;
+            } else if (skip == 1) {
+                right -= 1;
+                skip -= 1;
+            } else {
+                rightShift = false;
+                break;
+            }
+        }
+        
+        return leftShift || rightShift;
+        // int[] left = new int[26];
+        // int[] right = new int[26];
+        
+        // int mid = s.length() / 2;
+
+        // for (int i = 0; i < mid; i++) {
+        //     left[s.charAt(i) - 'a'] += 1;
+        //     right[s.charAt(s.length() - i) - 'a'] += 1;
+        // }
+
+
+    }
+}
+
+// [1a, 2b, 0c, 0d]
+// [1a, 0b, 1c, 1d]
